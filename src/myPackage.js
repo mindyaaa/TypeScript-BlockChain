dd4075d143830402f7d41db64c0b30ca73d608bf
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-01-30 15:55:39 +0900

    add js.Doc

---
+++ src/myPackage.js
@@ -0,0 +1,23 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns {void}
+ */
+
+export function init(config) {
+    return true;
+}
+
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns  {number}
+ */
+
+export function exit(code) {
+    return code + 1;
+}
+
