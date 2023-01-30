bdb5dc1c2e136d7d1aa73fdfe999ac2207bd3f3c
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-01-30 15:12:50 +0900

    lib 설정

--- build/index.js
+++ build/index.js
@@ -1,1 +1,12 @@
-const hello = () => "hi";
+class TV {
+    name : "";
+    price : 0;
+    size : "";
+    constructor(name, price, size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+}
+
+let tv1 = new TV('Sony', 100, '10inch');
